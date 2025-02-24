c768103463cf91b389e1c24b052c89d62462a6ec
firdaus <mfirdausraza.it@gmail.com>
2025-02-24 21:05:42 +0530

    sout message

--- src/main/java/com/custom/servalet_filter/ServletsFilterApplication.java
+++ src/main/java/com/custom/servalet_filter/ServletsFilterApplication.java
@@ -8,6 +8,7 @@ public class ServletsFilterApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServletsFilterApplication.class, args);
+		System.out.println("ServletsFilterApplication start successfully");
 	}
 
 }
